a976d06b1fa2944a28f80de9fae98d8f52d4b10f
unknown <dattatray.bhairi@critical-facility.com>
2022-08-01 16:33:37 +0530

    kafka consumer commit

--- lib/reader.go
+++ lib/reader.go
@@ -24,20 +24,21 @@ func KF_Consume(ctx context.Context, topic string) {
 	// the groupID identifies the consumer and prevents
 	// it from receiving duplicate messages
 	r := kafkago.NewReader(kafkago.ReaderConfig{
-		Brokers: []string{brokerAddress},
-		Topic:   "test",
-		GroupID: "my-group",
+		Brokers:   []string{brokerAddress},
+		Topic:     topic,
+		Partition: 0,
+		// GroupID: "my-group",
 		// assign the logger to the reader
 		Logger: l,
 	})
 
-	r1 := kafkago.NewReader(kafkago.ReaderConfig{
-		Brokers: []string{brokerAddress},
-		Topic:   "datta",
-		GroupID: "my-group",
-		// assign the logger to the reader
-		Logger: l,
-	})
+	// r1 := kafkago.NewReader(kafkago.ReaderConfig{
+	// 	Brokers: []string{brokerAddress},
+	// 	Topic:   "datta",
+	// 	// GroupID: "my-group",
+	// 	// assign the logger to the reader
+	// 	Logger: l,
+	// })
 
 	for {
 		// the `ReadMessage` method blocks until we receive the next event
@@ -46,13 +47,15 @@ func KF_Consume(ctx context.Context, topic string) {
 			fmt.Println("could not read message " + err.Error())
 		}
 		// after receiving the message, log its value
-		fmt.Println("received: ", string(msg.Value))
+		fmt.Println("received: ", string(msg.Value), "| topic : ", string(msg.Topic), "| Partition: ", string(msg.Partition))
+		// r.Close()
 
-		msg1, err1 := r1.ReadMessage(ctx)
-		if err1 != nil {
-			fmt.Println("could not read message " + err1.Error())
-		}
-		// after receiving the message, log its value
-		fmt.Println("received: ", string(msg1.Value))
+		// msg1, err1 := r1.ReadMessage(ctx)
+		// if err1 != nil {
+		// 	fmt.Println("could not read message " + err1.Error())
+		// }
+		// // after receiving the message, log its value
+		// fmt.Println("received: ", string(msg1.Value))
+		// r1.Close()
 	}
 }
--- main.go
+++ main.go
@@ -5,19 +5,19 @@ import (
 	"kafka/lib"
 )
 
-var topics = []string{"datta", "test"}
+var topics = []string{"datta", "test", "mcst"}
 
 func main() {
 	// create a new context
-	var s string = "Hello World"
-	sb := []byte(s)
+	// var s string = "Hello World"
+	// sb := []byte(s)
 	ctx := context.Background()
 	// produce messages in a new go routine, since
 	// both the produce and consume functions are
 	// blocking
 	lib.CreateTopics(ctx, topics)
-	go lib.KF_Produce(ctx, sb, "test")
-	lib.KF_Consume(ctx, "datta")
-	// lib.KF_Consume(ctx, "test")
+	// go lib.KF_Produce(ctx, sb, "test")
+	// go lib.KF_Consume(ctx, "datta")
+	lib.KF_Consume(ctx, "test")
 
 }
